59491af525c625624162f4254fa1345b2b658d04
evanina <eva.degano@gmail.com>
2021-10-18 17:23:35 +0200

    changed output folder

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import base64
 import os
 import hashlib
 import pandas as pd
@@ -8,9 +9,6 @@ import pandas as pd
 def main():
   # add an a title to the app
   st.title("File Hash")
-
-  # ask user for output folder 
-  folder_output = st.text_input("folder destination", "~/Downloads")
 
   # add a file picker
   uploaded_file = st.file_uploader("Upload CSV", type=['csv'])
@@ -25,8 +23,7 @@ def main():
     if uploaded_file is not None:
       for target in targets:
         if target in uploaded_file.name.lower():
-            #target_output = os.path.join(folder_output, f"{target}_init.csv")
-            target_output = folder_output
+            target_output = f"{target}_init.csv"
             my_hash(uploaded_file, target_output)
 
 
@@ -46,7 +43,14 @@ def my_hash(uploaded_file, target_output):
   df["md5"] = df.md5.apply(lambda x: hashlib.md5(x.encode()).hexdigest())
 
   # export hashed csv
-  df.to_csv(target_output, index = False)
+  csv = df.to_csv(target_output, index = False)
+
+  # byte conversion necessary for download
+  b64 = base64.b64encode(csv.encode()).decode()
+
+  # download file
+  href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+  st.markdown(href, unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
